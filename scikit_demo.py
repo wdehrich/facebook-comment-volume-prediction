e848b962184c2b4947bfb5311048ad24650a692c
Yixi Chen <ecchen96153@gmail.com>
2017-05-05 15:13:15 -0500

    test

--- scikit_demo.py
+++ scikit_demo.py
@@ -10,7 +10,7 @@ y = boston.target
 # cross_val_predict returns an array of the same size as `y` where each entry
 # is a prediction obtained by cross validation:
 predicted = cross_val_predict(lr, boston.data, y, cv=10)
-
+#add comment
 fig, ax = plt.subplots()
 ax.scatter(y, predicted)
 ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
