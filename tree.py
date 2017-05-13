166658040363740d51e7dab09ca1eec6332f5546
William Ehrich <wdehrich@gmail.com>
2017-05-13 01:41:09 -0500

    Added a URL prefix

--- tree.py
+++ tree.py
@@ -10,11 +10,12 @@ from sklearn.tree import DecisionTreeRegressor
 import utils as ut
 import matplotlib.pyplot as plt
 
+url_prefix = "https://raw.githubusercontent.com/wdehrich/facebook-comment-volume-prediction/master/Dataset/Dataset/"
 # training data URL
-url_train = "https://raw.githubusercontent.com/wdehrich/facebook-comment-volume-prediction/master/Dataset/Dataset/Training/Features_Variant_1.csv"
+url_train = url_prefix + "Training/Features_Variant_1.csv"
 # testing data URLs
 url_test = list()
-url_test.append("https://raw.githubusercontent.com/wdehrich/facebook-comment-volume-prediction/master/Dataset/Dataset/Testing/TestSet/Test_Case_1.csv")
+url_test.append(url_prefix + "Testing/TestSet/Test_Case_1.csv")
 url_test.append("https://raw.githubusercontent.com/wdehrich/facebook-comment-volume-prediction/master/Dataset/Dataset/Testing/TestSet/Test_Case_2.csv")
 url_test.append("https://raw.githubusercontent.com/wdehrich/facebook-comment-volume-prediction/master/Dataset/Dataset/Testing/TestSet/Test_Case_3.csv")
 url_test.append("https://raw.githubusercontent.com/wdehrich/facebook-comment-volume-prediction/master/Dataset/Dataset/Testing/TestSet/Test_Case_4.csv")
@@ -70,5 +71,4 @@ plt.xticks(y_pos, x)
 plt.ylabel('Score')
 plt.xlabel('Test')
 plt.title('Hits@10')
-
 plt.show()
