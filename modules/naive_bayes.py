2b7cc46c014dd17b9a924021cc4362904e4eaa17
Yixi Chen <ecchen96153@gmail.com>
2017-06-02 21:06:09 -0500

    implement nb on relative features

--- modules/naive_bayes.py
+++ modules/naive_bayes.py
@@ -3,50 +3,47 @@ import numpy as np
 import urllib
 import utils as ut
 import matplotlib.pyplot as plt
-url_prefix = "https://raw.githubusercontent.com/wdehrich/facebook-comment-volume-prediction/master/Dataset/Dataset/"
-# training data URL
-url_train = url_prefix + "Training/Features_Variant_1.csv"
-# testing data URLs
-url_test = list()
-url_test.append(url_prefix + "Testing/TestSet/Test_Case_1.csv")
-url_test.append(url_prefix + "Testing/TestSet/Test_Case_2.csv")
-url_test.append(url_prefix + "Testing/TestSet/Test_Case_3.csv")
-url_test.append(url_prefix + "Testing/TestSet/Test_Case_4.csv")
-url_test.append(url_prefix + "Testing/TestSet/Test_Case_5.csv")
-url_test.append(url_prefix + "Testing/TestSet/Test_Case_6.csv")
-url_test.append(url_prefix + "Testing/TestSet/Test_Case_7.csv")
-url_test.append(url_prefix + "Testing/TestSet/Test_Case_8.csv")
-url_test.append(url_prefix + "Testing/TestSet/Test_Case_9.csv")
-url_test.append(url_prefix + "Testing/TestSet/Test_Case_10.csv")
 
+def get_hits_at_ten(actual, predicted):
+	order_actual = np.argsort(actual)
+	order_predicted = np.argsort(predicted)
+	top_ten_actual = order_actual[-10:]
+	top_ten_predicted = order_predicted[-10:]
+	commonalities = np.intersect1d(top_ten_predicted, top_ten_actual)
+	hits_at_ten = len(commonalities)
+	return hits_at_ten
+
+# url_train, url_test = ut.get_urls("features-1")
+# url_train, url_test = ut.get_urls("features-2")
+url_train, url_test = ut.get_urls("features-3")
 # download training data file
 raw_data = urllib.urlopen(url_train)
 # load the CSV file as a numpy matrix
 data_set_train = np.loadtxt(raw_data, delimiter=",", skiprows=1)
 # separate the data from the target attributes
-X = data_set_train[:, 0:52]
-y = data_set_train[:, 53]
+num_attributes = len(data_set_train[0]) - 1
+X = data_set_train[:, 0:(num_attributes - 1)]
+y = data_set_train[:, num_attributes]
 clf = GaussianNB()
 clf.fit(X,y)
-num_url_test = len(url_test)
 hits_at_ten = list()
-
+num_url_test = len(url_test)
+# download the testing data file
 for i in range(num_url_test):
-    current_url_test = url_test[i]
-    # download the testing data file
-    raw_data = urllib.urlopen(current_url_test)
-    # load the CSV file as a numpy matrix
-    data_set_test = np.loadtxt(raw_data, delimiter=",")
+	current_url_test = url_test[i]
+	raw_data = urllib.urlopen(current_url_test)
+	# load the CSV file as a numpy matrix
+	data_set_test = np.loadtxt(raw_data, delimiter=",",skiprows=1)
 
-    # Predict
-    X_test = data_set_test[:, 0:52]
-    y_test = data_set_test[:, 53]
-    y_predicted = clf.predict(X_test)
-    # Get Hits@10 measurement
-    hits_at_ten_current = ut.get_hits_at_ten(y_test, y_predicted)
-    hits_at_ten.append(hits_at_ten_current)
+	# Predict
+	X_test = data_set_test[:, 0:(num_attributes - 1)]
+	y_test = data_set_test[:, num_attributes]
+	y_predicted = clf.predict(X_test)
+	# Get Hits@10 measurement
+	hits_at_ten_current = get_hits_at_ten(y_test, y_predicted)
+	hits_at_ten.append(hits_at_ten_current)
 
-hits_at_ten_average = float(sum(hits_at_ten)/float(num_url_test))
+	hits_at_ten_average = float(sum(hits_at_ten)/float(num_url_test))
 print 'hits_at_ten =', hits_at_ten
 print 'hit_at_ten_average =', hits_at_ten_average
 
