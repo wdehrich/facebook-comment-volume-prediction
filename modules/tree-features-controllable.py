d2d12a48c9fb68ad98baa615517a433d414f387e
William Ehrich <wdehrich@gmail.com>
2017-06-03 14:44:45 -0500

    Added tree experiement with controllable features

--- modules/tree-features-controllable.py
+++ modules/tree-features-controllable.py
@@ -3,7 +3,7 @@ import accuracy as ac
 
 data_set_train, data_sets_test = ut.get_data()
 
-columns = [30, 35, 53]
+columns = [30, 35, 39, 40, 41, 42, 43, 44, 45, 53]
 data_set_train_selected, data_sets_test_selected = ut.select_data(data_set_train, data_sets_test, columns)
 
 ac.get_accuracy(data_set_train_selected, data_sets_test_selected)
