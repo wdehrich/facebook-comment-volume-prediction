1654c1ad3ddca55e6e11a86bf8aa651761814983
Chungyuen Li <lizoyu1@gmail.com>
2017-06-03 07:53:09 -0500

    Revert "Revert "add a variable importance plot""
    This reverts commit b05fe373fbc1909dc2f374e9002b11b071c3ef7f.

--- modules/tester_neuralnet.py
+++ modules/tester_neuralnet.py
@@ -1,25 +1,27 @@
 import matplotlib.pyplot as plt
 import neural_net as nn
 import numpy as np
+import accuracy as acc
 import utils
 
 # get data
 attr, label = utils.parse('E:/Features_Variant_1.csv')
-attr_test, target = utils.parse('E:/Test_Case.csv', False)
+attr_test, target = utils.parse('E:/Test_Case_all.csv', True)
 
 # train
 mlp, scaler = nn.mlp_train(attr, label)
 
 # mean square error
-print 'mean square error: ', utils.mse(target,mlp.predict(scaler.transform(attr_test)))
+print 'mean square error: ', acc.mse(target,mlp.predict(scaler.transform(attr_test)))
 
 # actual-perdicted ratio
-#utils.ratio(target, mlp.predict(scaler.transform(attr_test)), 2)
+#acc.ratio(target, mlp.predict(scaler.transform(attr_test)), 2)
 
-# hit @ 10
+# hits@10
 hit_10 = []
 for i in np.arange(0,1000,100):
-	hit_10.append(utils.get_hits_at_ten(target[i:i+100],mlp.predict(scaler.transform(attr_test[i:i+100]))))
+	hit_10.append(acc.get_hits_at_ten(target[i:i+100],mlp.predict(scaler.transform(attr_test[i:i+100]))))
+print 'Hit@10 average: ', np.mean(hit_10)
 hit_10.append(np.mean(hit_10))
 
 x = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Avg.']
