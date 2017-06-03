ee4b1ff99c88f91adfa9ddd6df7d155a9c13c7ff
William Ehrich <wdehrich@gmail.com>
2017-06-03 15:11:15 -0500

    Removed old files

--- modules/tree-features-phenomenon.py
+++
@@ -1,9 +0,0 @@
-import utils as ut
-import accuracy as ac
-
-data_set_train, data_sets_test = ut.get_data()
-
-columns = [30, 34, 36, 53]
-data_set_train_selected, data_sets_test_selected = ut.select_data(data_set_train, data_sets_test, columns)
-
-ac.get_accuracy(data_set_train_selected, data_sets_test_selected)
